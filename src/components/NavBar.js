6589088f1f0b4d8fe399903953d6e0e8b90bbb3a
Manikanta954 <talagamamanikanta98@gmail.com>
2024-07-14 17:20:55 +0530

    made some changes again

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'; // Import MUI icon for cart
 import './Navbar.css';
 
-const Navbar = () => {
+const Navbar = ({ cartItemsCount }) => {
   return (
     <nav className="navbar">
-      <div className="company-name">Organic Farm</div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/products">Products</Link></li>
-        <li><Link to="/cart">Cart</Link></li>
-        <li><Link to="/profile">Profile</Link></li>
-      </ul>
+      <div className="logo" >
+      Organic Farm
+      </div>
+      <div className="nav-links">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/products">Products</Link></li>
+          <li><Link to="/cart">
+            <ShoppingCartIcon /> {/* Display cart icon */}
+            {cartItemsCount > 0 && <span className="cart-count">{cartItemsCount}</span>} {/* Display count if > 0 */}
+          </Link></li>
+          <li><Link to="/profile">Profile</Link></li>
+        </ul>
+      </div>
     </nav>
   );
 };
